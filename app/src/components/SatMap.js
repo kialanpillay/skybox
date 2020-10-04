63518d62a6b01449d1210ed5929a44caedfe4eb3
Kialan Pillay <kialanpillay@gmail.com>
2020-10-04 22:21:38 +0200

    Updated marker icon

--- app/src/components/SatMap.js
+++ app/src/components/SatMap.js
@@ -3,7 +3,7 @@ import { Map, Marker, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 
 const icon = new Icon({
-  iconUrl: "FP_Satellite_icon.svg",
+  iconUrl: "Satellite_icon.png",
   iconSize: [40, 40],
 });
 
@@ -11,7 +11,7 @@ export default function SatMap(props) {
   const position = [props.location.lat, props.location.lng];
   return (
     <Map center={position} zoom={1} zoomControl={false}>
-      <TileLayer url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png" />
+      <TileLayer url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png" />
       <Marker position={position} icon={icon}></Marker>
     </Map>
   );
